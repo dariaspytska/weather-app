309f59ae3e40e5451f2942bda0dde96203d3fe0f
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2022-06-04 14:27:29 +0300

    add date

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
+function addDate(dayTime) {
+   let date = new Date(dayTime);
+   let day = date.getDay();
+   let hours = date.getHours();
+   let minutes = date.getMinutes();
+   return `${day} ${hours}:${minutes}` ;
+}
 function showTemp(resp) {
-  console.log(resp);
+  //console.log(resp);
   let tempElement = document.querySelector("#temperature");
   tempElement.innerHTML = Math.round(resp.data.main.temp);
   let cityElement = document.querySelector("#city");
@@ -10,8 +17,11 @@ function showTemp(resp) {
   humidityElement.innerHTML = resp.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(resp.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = addDate(resp.data.dt * 1000);
+  
 }
 
 let apiKey = "9eac88714aa707593c33f048a9d7da34";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Zaporizhzhya&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemp);
