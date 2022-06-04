f37db4d41b921f4ca3de5399a29a12ca32da8557
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2022-06-04 20:12:19 +0300

    add icon for weather response

--- src/app.js
+++ src/app.js
@@ -1,27 +1,48 @@
-function addDate(dayTime) {
-   let date = new Date(dayTime);
-   let days = [
-     "Sunday",
-     "Monday",
-     "Tuesday",
-     "Wednesday",
-     "Thursday",
-     "Friday",
-     "Saturday",
-   ];
-   let day = days[date.getDay()];
-   let hours = date.getHours();
-   if (hours < 10) {
-     hours = `0${hours}`;
-   }
-   let minutes = date.getMinutes();
-   if (minutes < 10) {
-     minutes = `0${minutes}`;
-   }
-   return `${day} ${hours}:${minutes}` ;
+
+function iconShow(iconName) {
+  let icon = iconName;
+  if (icon === "Thunderstorm") {
+    return "img/thunderstorm.7e644c1a.svg";
+  } else if (icon === "Clear") {
+    return "img/sun (1).png";
+  } else if (icon === "Clouds") {
+    return "img/cloud.2b02f907.svg";
+  } else if (icon === "Drizzle") {
+    return "img/rain.a56a7915.svg";
+  } else if (icon === "Rain") {
+    return "img/rain.a56a7915.svg";
+  } else if (icon === "Snow") {
+    return "img/snow.0bb8cb16.svg";
+  } else {
+    return "img/cloud.2b02f907.svg";
+  }
 }
+function addDate(dayTime) {
+  let date = new Date(dayTime);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemp(resp) {
-  //console.log(resp);
+  console.log(resp);
   let tempElement = document.querySelector("#temperature");
   tempElement.innerHTML = Math.round(resp.data.main.temp);
   let cityElement = document.querySelector("#city");
@@ -34,10 +55,12 @@ function showTemp(resp) {
   windElement.innerHTML = Math.round(resp.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = addDate(resp.data.dt * 1000);
-  
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `${iconShow(resp.data.weather[0].main)}`);
 }
 
+
 let apiKey = "9eac88714aa707593c33f048a9d7da34";
-let city = "Paris";
+let city = "Tokmak";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemp);
