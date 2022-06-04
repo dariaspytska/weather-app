6333d8ba0ef5ab4ecc0cf0354f79e4be3a6a491d
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2022-06-04 15:07:58 +0300

    Fix date+time showing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 function addDate(dayTime) {
    let date = new Date(dayTime);
-   let day = date.getDay();
+   let days = [
+     "Sunday",
+     "Monday",
+     "Tuesday",
+     "Wednesday",
+     "Thursday",
+     "Friday",
+     "Saturday",
+   ];
+   let day = days[date.getDay()];
    let hours = date.getHours();
+   if (hours < 10) {
+     hours = `0${hours}`;
+   }
    let minutes = date.getMinutes();
+   if (minutes < 10) {
+     minutes = `0${minutes}`;
+   }
    return `${day} ${hours}:${minutes}` ;
 }
 function showTemp(resp) {
